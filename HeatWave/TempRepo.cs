02d90ea13e9a7a6bf225c69ed3a5a888c2e0be0a
AlexanderHoeystad <Alek369@hotmail.com>
2024-05-08 12:00:15 +0200

    Test first repo

---
+++ HeatWave/TempRepo.cs
@@ -0,0 +1,23 @@
+﻿namespace HeatWave
+{
+    public class TempRepo
+    {
+        private List<TemperatureMeasurement> _TempList = new List<TemperatureMeasurement>
+        {
+            new TemperatureMeasurement { Id = 1, InDoorTemperature = 20, OutDoorTemperature = 25, Date = new DateTime(2021, 5, 10, 8, 38, 16) },
+            new TemperatureMeasurement { Id = 2, InDoorTemperature = 21, OutDoorTemperature = 26, Date = new DateTime(2021, 5, 11, 8, 38, 16) },
+            new TemperatureMeasurement { Id = 3, InDoorTemperature = 22, OutDoorTemperature = 27, Date = new DateTime(2021, 5, 12, 8, 38, 16) },
+            new TemperatureMeasurement { Id = 4, InDoorTemperature = 23, OutDoorTemperature = 28, Date = new DateTime(2021, 5, 13, 8, 38, 16) },
+            new TemperatureMeasurement { Id = 5, InDoorTemperature = 24, OutDoorTemperature = 29, Date = new DateTime(2021, 5, 14, 8, 38, 16) }
+        };
+
+        public List<TemperatureMeasurement> GetTempList()
+        {
+            return _TempList;
+        }
+
+
+
+
+    }
+}
---
+++ HeatWaveTests/TempRepoTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HeatWave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeatWave.Tests
+{
+    [TestClass()]
+    public class TempRepoTests
+    {
+        TempRepo _Templist = new TempRepo();
+        TemperatureMeasurement tM = new TemperatureMeasurement { Id = 6, InDoorTemperature = 17, OutDoorTemperature = 23, Date = new DateTime(2022, 6, 11, 10, 39, 12) };
+
+
+        [TestMethod()]
+        public void GetTempListTest()
+        {
+            var TempMeasurement = _Templist.GetTempList();
+            Assert.AreEqual(5, TempMeasurement.Count);
+
+
+        }
+
+        public void GetId()
+        {
+            TemperatureMeasurement? tM = _Templist.GetTempID(1);
+            Assert.IsNotNull(tM);
+            Assert.AreEqual(1, tM.Id);
+            Assert.IsNull(_Templist.GetTempID(100));
+
+
+        }
+
+        public void AddTempMeasurementTest()
+        {
+            _Templist.AddTempMeasurement(tM);
+            Assert.AreEqual(6, _Templist.GetTempList().Count);
+            Assert.AreEqual(6, _Templist.GetTempID(6).Id);
+        }
+
+        public void RemoveTempMeasurementTest()
+        {
+            _Templist.RemoveTempMeasurement(5);
+            Assert.AreEqual(4, _Templist.GetTempList().Count);
+            Assert.IsNull(_Templist.GetTempID(5));
+
+        }
+
+        public void UpdateTempMeasurementTest()
+        {
+            _Templist.UpdateTempMeasurement(1, tM);
+            Assert.IsNotNull(_Templist.GetTempID(1));
+            Assert.AreEqual(17.0, _Templist.GetTempID(1).InDoorTemperature);
+            Assert.AreEqual(23.0, _Templist.GetTempID(1).OutDoorTemperature);
+            Assert.AreEqual(new DateTime(2022, 6, 11, 10, 39, 12), _Templist.GetTempID(1).Date);
+
+        }
+
+        public override string ToString()
+        {
+            _Templist.GetTempList();
+            Assert.AreEqual("Id: 1, InDoorTemperature: 20, OutDoorTemperature: 25, Date: 10-05-2021 08:38:16", _Templist.ToString());
+        }
+
+
+    }
+}
