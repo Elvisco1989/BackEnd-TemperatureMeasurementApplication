27c90f136da701c4b32f7742f252d8f2ebfa07dc
imeugenecarlo <beugenecarlo@gmail.com>
2024-05-08 12:55:50 +0200

    Ændret Repo tilføjet metoder

--- HeatWave/TempRepo.cs
+++ HeatWave/TempRepo.cs
@@ -2,6 +2,7 @@
 {
     public class TempRepo
     {
+        private int _nextId;
         private List<TemperatureMeasurement> _TempList = new List<TemperatureMeasurement>
         {
             new TemperatureMeasurement { Id = 1, InDoorTemperature = 20, OutDoorTemperature = 25, Date = new DateTime(2021, 5, 10, 8, 38, 16) },
@@ -11,13 +12,93 @@
             new TemperatureMeasurement { Id = 5, InDoorTemperature = 24, OutDoorTemperature = 29, Date = new DateTime(2021, 5, 14, 8, 38, 16) }
         };
 
-        public List<TemperatureMeasurement> GetTempList()
+        public IEnumerable<TemperatureMeasurement> GetTempList(DateTime? date = null, string? orderby = null)
         {
-            return _TempList;
+            IEnumerable<TemperatureMeasurement> result = new List<TemperatureMeasurement>(_TempList);
+            if (date != null)
+            {
+                result = result.Where(tM => tM.Date == date.Value.Date);
+            }
+
+            if (orderby != null)
+            {
+                switch (orderby)
+                {
+                    case "InDoorTemperature":
+                        result = result.OrderBy(tM => tM.InDoorTemperature);
+                        break;
+                    case "OutDoorTemperature":
+                        result = result.OrderBy(tM => tM.OutDoorTemperature);
+                        break;
+                    case "Date":
+                        result = result.OrderBy(tM => tM.Date);
+                        break;
+                    case "Id":
+                        result = result.OrderBy(tM => tM.Id);
+                        break;
+                    case "InDoorTemperatureDesc":
+                        result = result.OrderByDescending(tM => tM.InDoorTemperature);
+                        break;
+                    case "OutDoorTemperatureDesc":
+                        result = result.OrderByDescending(tM => tM.OutDoorTemperature);
+
+                        break;
+                    case "DateDesc":
+                        result = result.OrderByDescending(tM => tM.Date);
+                        break;
+                    case "IdDesc":
+                        result = result.OrderByDescending(tM => tM.Id);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+
         }
 
+        public TemperatureMeasurement? GetID(int id)
+        {
 
+            return _TempList.Find(tM => tM.Id == id);
+        }
 
+        public TemperatureMeasurement Add(TemperatureMeasurement tM)
+        {
+            tM.Validate();
+            tM.Id = _nextId++;
+            _TempList.Add(tM);
+            return tM;
+        }
 
+        public TemperatureMeasurement Remove(int Id)
+        {
+            TemperatureMeasurement? existingtM = GetID(Id);
+            if (existingtM == null)
+            {
+                return null;
+            }
+            _TempList.Remove(existingtM);
+            return existingtM;
+        }
+
+        public TemperatureMeasurement Update(int Id, TemperatureMeasurement tM)
+        {
+            tM.Validate();
+            TemperatureMeasurement? existingtM = GetID(Id);
+            if (existingtM == null)
+            {
+                return null;
+            }
+            existingtM.InDoorTemperature = tM.InDoorTemperature;
+            existingtM.OutDoorTemperature = tM.OutDoorTemperature;
+            existingtM.Date = tM.Date;
+            return existingtM;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n ", _TempList);
+        }
     }
 }
