21a5576f31f4f1269490389c2ba5d2cc9acb8d61
AlexanderHoeystad <Alek369@hotmail.com>
2024-05-22 10:34:18 +0200

    Database test setup

--- HeatWaveTests/TempRepoDBTests.cs
+++ HeatWaveTests/TempRepoDBTests.cs
@@ -5,12 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace HeatWave.Tests
 {
     [TestClass()]
     public class TempRepoDBTests
     {
+
+        private const bool useDatabase = true;
+        private static TempRepoDB _repoDB;
+
+        [ClassInitialize]
+
+        public static void InitializeOnce(TestContext context)
+        {
+            if (useDatabase)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<TempDBContext>();
+
+                optionsBuilder.UseSqlServer(DBSecret.ConnectionStringSimply);
+
+                TempDBContext _tempDBContext = new(optionsBuilder.Options);
+
+                _tempDBContext.Database.ExecuteSqlRaw("TRUNCATE TABLE dbo.TemperatureMeasurement");
+
+                _repoDB = new TempRepoDB(_tempDBContext);
+            }
+        }
+
         [TestMethod()]
         public void TempRepoDBTest()
         {
