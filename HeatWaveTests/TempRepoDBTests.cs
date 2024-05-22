50b042a8548d218c35a679463e84f8a5520ccc08
AlexanderHoeystad <Alek369@hotmail.com>
2024-05-22 12:46:48 +0200

    Database test 100 procent færdig

--- HeatWaveTests/TempRepoDBTests.cs
+++ HeatWaveTests/TempRepoDBTests.cs
@@ -13,37 +13,36 @@ namespace HeatWave.Tests
     [TestClass()]
     public class TempRepoDBTests
     {
-
         private const bool useDatabase = true;
-        private static TempRepoDB _repoDB;
+        private static DbContextOptions<TempDBContext> _options;
 
         [ClassInitialize]
-
         public static void InitializeOnce(TestContext context)
         {
             if (useDatabase)
             {
                 var optionsBuilder = new DbContextOptionsBuilder<TempDBContext>();
-
                 optionsBuilder.UseSqlServer(DBSecret.ConnectionStringSimply);
+                _options = optionsBuilder.Options;
 
-                TempDBContext _tempDBContext = new(optionsBuilder.Options);
-
+                using var _tempDBContext = new TempDBContext(_options);
                 _tempDBContext.Database.ExecuteSqlRaw("TRUNCATE TABLE dbo.TemperatureMeasurement");
-
-                _repoDB = new TempRepoDB(_tempDBContext);
             }
         }
 
-        [TestMethod()]
-        public void TempRepoDBTest()
+        [TestInitialize]
+        public void TestInitialize()
         {
-            Assert.Fail();
+            using var _tempDBContext = new TempDBContext(_options);
+            _tempDBContext.Database.ExecuteSqlRaw("TRUNCATE TABLE dbo.TemperatureMeasurement");
         }
 
         [TestMethod()]
         public void GetTempListTest()
         {
+            using var _tempDBContext = new TempDBContext(_options);
+            var _repoDB = new TempRepoDB(_tempDBContext);
+
             IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 0);
 
@@ -55,25 +54,28 @@ namespace HeatWave.Tests
             Assert.IsTrue(result.First().InDoorTemperature == 11);
             Assert.IsTrue(result.First().OutDoorTemperature == 12);
             Assert.IsTrue(result.First().Date == new DateTime(2021, 5, 10, 8, 38, 0));
-
         }
 
         [TestMethod()]
         public void GetIDTest()
         {
-            TemperatureMeasurement tmAdd = _repoDB.Add( new TemperatureMeasurement { Date = new DateTime(2022, 6, 11, 7, 32, 0), InDoorTemperature = 9, OutDoorTemperature = 8 });
+            using var _tempDBContext = new TempDBContext(_options);
+            var _repoDB = new TempRepoDB(_tempDBContext);
+
+            TemperatureMeasurement tmAdd = _repoDB.Add(new TemperatureMeasurement { Date = new DateTime(2022, 6, 11, 7, 32, 0), InDoorTemperature = 9, OutDoorTemperature = 8 });
             TemperatureMeasurement? tm = _repoDB.GetID(tmAdd.Id);
-            Assert.IsTrue(tm.Id == 1);
+            Assert.IsTrue(tm.Id == tmAdd.Id);
             Assert.IsTrue(tm.InDoorTemperature == 9);
             Assert.IsTrue(tm.OutDoorTemperature == 8);
             Assert.IsTrue(tm.Date == new DateTime(2022, 6, 11, 7, 32, 0));
-
-
         }
 
         [TestMethod()]
         public void AddTest()
         {
+            using var _tempDBContext = new TempDBContext(_options);
+            var _repoDB = new TempRepoDB(_tempDBContext);
+
             TemperatureMeasurement temperatureMeasurementMinus = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = -17, OutDoorTemperature = -17 };
             _repoDB.Add(temperatureMeasurementMinus);
             Assert.IsTrue(temperatureMeasurementMinus.InDoorTemperature < 0);
@@ -81,12 +83,14 @@ namespace HeatWave.Tests
             Assert.IsTrue(temperatureMeasurementMinus.Id > 0);
             IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 1);
-
         }
 
         [TestMethod()]
         public void DeleteTest()
         {
+            using var _tempDBContext = new TempDBContext(_options);
+            var _repoDB = new TempRepoDB(_tempDBContext);
+
             TemperatureMeasurement temperatureMeasurement = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 11, OutDoorTemperature = 12 };
             _repoDB.Add(temperatureMeasurement);
             IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
@@ -94,26 +98,24 @@ namespace HeatWave.Tests
             _repoDB.Delete(temperatureMeasurement.Id);
             result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 0);
-
         }
 
         [TestMethod()]
         public void UpdateTest()
         {
+            using var _tempDBContext = new TempDBContext(_options);
+            var _repoDB = new TempRepoDB(_tempDBContext);
+
             TemperatureMeasurement tM = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 11, OutDoorTemperature = 12 };
             _repoDB.Add(tM);
-            IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
+            var result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 1);
-            tM.InDoorTemperature = 13;
-            tM.OutDoorTemperature = 14;
-            tM.Date = new DateTime(2021, 5, 10, 8, 38, 0);
-            _repoDB.Update(tM);
+            TemperatureMeasurement tMUpdated = new TemperatureMeasurement { Id = tM.Id, Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 13, OutDoorTemperature = 14 };
+            _repoDB.Update(tM.Id, tMUpdated);
             result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 1);
             Assert.IsTrue(result.First().InDoorTemperature == 13);
             Assert.IsTrue(result.First().OutDoorTemperature == 14);
-            Assert.IsTrue(result.First().Date == new DateTime(2021, 5, 10, 8, 38, 0));
-
         }
     }
 }
