665a141b7581c4bd3dbf2869b1dfe2dcf4dc6410
Nuggitt <Phulip.nielsen@gmail.com>
2024-05-23 12:56:58 +0200

    virker det nu?

---
+++ HeatWave/DBSecret.cs
@@ -0,0 +1,10 @@
+﻿namespace HeatWave
+{
+    public class DBSecret
+    {
+
+        public static readonly string ConnectionStringSimply =
+            "Data Source=myzealandstudioserver.database.windows.net;Initial Catalog=mystudioDB;User ID=phil2629;Password=Holger1208!;Connect Timeout=60;Trust Server Certificate=True";
+
+    }
+}
