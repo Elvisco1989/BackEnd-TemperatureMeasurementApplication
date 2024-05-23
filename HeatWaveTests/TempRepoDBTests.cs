84f82b021667bbccd7f61263dfa227a42871824d
AlexanderHoeystad <Alek369@hotmail.com>
2024-05-23 10:32:15 +0200

    Refactoreret med kommentar på DB Test

--- HeatWaveTests/TempRepoDBTests.cs
+++ HeatWaveTests/TempRepoDBTests.cs
@@ -17,6 +17,7 @@ namespace HeatWave.Tests
         private static DbContextOptions<TempDBContext> _options;
 
         [ClassInitialize]
+
         public static void InitializeOnce(TestContext context)
         {
             if (useDatabase)
@@ -30,6 +31,7 @@ namespace HeatWave.Tests
             }
         }
 
+        // Tømmer databasen før hver test
         [TestInitialize]
         public void TestInitialize()
         {
@@ -37,19 +39,26 @@ namespace HeatWave.Tests
             _tempDBContext.Database.ExecuteSqlRaw("TRUNCATE TABLE dbo.TemperatureMeasurement");
         }
 
+        // Tester GetTempList metoden
         [TestMethod()]
         public void GetTempListTest()
         {
+            // Arrange - opretter en TempRepoDB og en TempDBContext.
             using var _tempDBContext = new TempDBContext(_options);
             var _repoDB = new TempRepoDB(_tempDBContext);
 
+            // Act - kalder GetTempList metoden
             IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
             Assert.IsTrue(result.Count() == 0);
 
+            // Assert - tester at der ikke er nogen elementer i listen
             TemperatureMeasurement temperatureMeasurement = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 11, OutDoorTemperature = 12 };
             _repoDB.Add(temperatureMeasurement);
 
+            // Act - kalder GetTempList metoden igen
             result = _repoDB.GetTempList();
+
+            // Assert - tester at der er et element i listen og at det er det rigtige elementer
             Assert.IsTrue(result.Count() == 1);
             Assert.IsTrue(result.First().InDoorTemperature == 11);
             Assert.IsTrue(result.First().OutDoorTemperature == 12);
@@ -62,8 +71,12 @@ namespace HeatWave.Tests
             using var _tempDBContext = new TempDBContext(_options);
             var _repoDB = new TempRepoDB(_tempDBContext);
 
+            // Act - adder et element til listen og kalder GetID metoden
             TemperatureMeasurement tmAdd = _repoDB.Add(new TemperatureMeasurement { Date = new DateTime(2022, 6, 11, 7, 32, 0), InDoorTemperature = 9, OutDoorTemperature = 8 });
             TemperatureMeasurement? tm = _repoDB.GetID(tmAdd.Id);
+
+            // Assert - tester at der er et element i listen og at det er det rigtige elementer
+            Assert.IsNotNull(tm);
             Assert.IsTrue(tm.Id == tmAdd.Id);
             Assert.IsTrue(tm.InDoorTemperature == 9);
             Assert.IsTrue(tm.OutDoorTemperature == 8);
@@ -76,8 +89,11 @@ namespace HeatWave.Tests
             using var _tempDBContext = new TempDBContext(_options);
             var _repoDB = new TempRepoDB(_tempDBContext);
 
+            // Act - adder et element til listen
             TemperatureMeasurement temperatureMeasurementMinus = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = -17, OutDoorTemperature = -17 };
             _repoDB.Add(temperatureMeasurementMinus);
+
+            // Assert - tester at der er et element i listen og at det er det rigtige elementer
             Assert.IsTrue(temperatureMeasurementMinus.InDoorTemperature < 0);
             Assert.IsTrue(temperatureMeasurementMinus.OutDoorTemperature < 0);
             Assert.IsTrue(temperatureMeasurementMinus.Id > 0);
@@ -91,6 +107,7 @@ namespace HeatWave.Tests
             using var _tempDBContext = new TempDBContext(_options);
             var _repoDB = new TempRepoDB(_tempDBContext);
 
+            // Act - adder et element til listen og sletter det igen
             TemperatureMeasurement temperatureMeasurement = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 11, OutDoorTemperature = 12 };
             _repoDB.Add(temperatureMeasurement);
             IEnumerable<TemperatureMeasurement> result = _repoDB.GetTempList();
@@ -106,6 +123,7 @@ namespace HeatWave.Tests
             using var _tempDBContext = new TempDBContext(_options);
             var _repoDB = new TempRepoDB(_tempDBContext);
 
+            // Act - adder et element til listen og updater det igen med nye værdier
             TemperatureMeasurement tM = new TemperatureMeasurement { Date = new DateTime(2021, 5, 10, 8, 38, 0), InDoorTemperature = 11, OutDoorTemperature = 12 };
             _repoDB.Add(tM);
             var result = _repoDB.GetTempList();
